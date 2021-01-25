db56492e3210aaa14d0545768c8de57adf76b806
Jaf <jaffreyrodriguez42@gmail.com>
2021-01-26 01:26:49 +0800

    JSX attributes and styling react elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-	const name = "Jaff";
-	
-	const d = new Date();
-	const year = d.getFullYear();
+	const img = "https://picsum.photos/200"; 
   return (
   	<div>
-  		<p>Created by {name}.</p>
-		<p>Copyright {year}.</p>
+  		<h1 className="heading" contentEditable="true" spellCheck="false">My Favorite Food.</h1> {/*attributes should be in camelcase*/}
+  		<div>
+  			<img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7oK8VZHySBRmUx8yfumUxz1U8KkxwPSAuQg&usqp=CAU" alt="bacon"/>
+  			<img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpFpOdV87bAeKsklcUb2AWPf6beuJjU3cB5w&usqp=CAU" alt="hotdog"/>
+  			<img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEKI_5VYs0UnIsfZPVLxOWVGcaNmLNn3BOuw&usqp=CAU" alt="cake"/>
+  			<img src={img + "?grayscale"} alt="lorempicsum"/>  {/*you can use javascript expressions as the value of the attributes*/}
+  		</div>
   	</div>
     
   );
