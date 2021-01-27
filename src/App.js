446744ed26e7ddcaec0955537f953d505e2e6397
Jaff <jaffreyrodriguez42@gmail.com>
2021-01-27 20:53:43 +0800

    Inline styling for react elements

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 
 
 function App() {
-	const img = "https://picsum.photos/200"; 
+  const customStyle = {
+    color: "red",
+    fontSize: "20px",
+    border: "1px solid black"
+  } 
+
+  customStyle.color = "blue";
+
   return (
-  	<div>
-  		<h1 className="heading" contentEditable="true" spellCheck="false">My Favorite Food.</h1> {/*attributes should be in camelcase*/}
-  		<div>
-  			<img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7oK8VZHySBRmUx8yfumUxz1U8KkxwPSAuQg&usqp=CAU" alt="bacon"/>
-  			<img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpFpOdV87bAeKsklcUb2AWPf6beuJjU3cB5w&usqp=CAU" alt="hotdog"/>
-  			<img className="food-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEKI_5VYs0UnIsfZPVLxOWVGcaNmLNn3BOuw&usqp=CAU" alt="cake"/>
-  			<img src={img + "?grayscale"} alt="lorempicsum"/>  {/*you can use javascript expressions as the value of the attributes*/}
-  		</div>
-  	</div>
+    <h1 style={customStyle}>Hello World!!</h1>
     
   );
 }
