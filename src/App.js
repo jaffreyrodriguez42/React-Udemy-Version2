1104d897b501365ac03d713390e4fc1a7509302f
Jaff <jaffreyrodriguez42@gmail.com>
2021-01-30 15:03:09 +0800

    React styling practice

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import './App.css';
 
 
 function App() {
-  const customStyle = {
-    color: "red",
-    fontSize: "20px",
-    border: "1px solid black"
-  } 
+  const d = new Date();
+  const currentHour = d.getHours();
 
-  customStyle.color = "blue";
+  let messageColor = {
+    color: ""
+  }
+  let message = "";
 
+
+
+
+  if(currentHour < 12){
+    messageColor.color = "red";
+    message = "Morning";
+  }else if(currentHour < 18){
+    messageColor.color = "green";
+    message = "Afternoon";
+  }else if(currentHour < 24){
+    messageColor.color = "blue";
+    message = "Night";
+  }
+
+  console.log(message);
+  console.log(currentHour);
+  console.log(messageColor.color);
   return (
-    <h1 style={customStyle}>Hello World!!</h1>
+    <h1 className="heading" style={messageColor}>{ "Good " + message + "!!"}</h1>
     
   );
 }
