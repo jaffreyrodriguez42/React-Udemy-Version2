c22135b4a7953ae14c55d529740638c89b8138b7
Jaff <jaffreyrodriguez42@gmail.com>
2021-01-31 17:47:11 +0800

    React components practice

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 
 function Heading(){
+
+	const d = new Date(); 
+	const currentHour = d.getHours();
+
+	 let messageColor = {
+	   color: ""
+	 }
+
+	 let message;
+
+	 if(currentHour < 12){
+	   messageColor.color = "red";
+	   message = "Morning";
+	 }else if(currentHour < 18){
+	   messageColor.color = "green";
+	   message = "Afternoon";
+	 }else if(currentHour < 24){
+	   messageColor.color = "blue";
+	   message = "Night";
+	 }
+
 	return(
-		<h1>My Favorite Foods</h1>
+		<h1 className="heading" style={messageColor}>{ "Good " + message + "!!"}</h1>
 	); 
 }
 
